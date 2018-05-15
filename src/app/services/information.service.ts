500b0d53d9cc713efc3233288be1290b3b3e824a
Carlos Nobre <ext.carlos.nobre@ingg.com>
2018-05-15 11:44:11 +0100

    Firebase Service ready

--- src/app/services/information.service.ts
+++ src/app/services/information.service.ts
@@ -6,14 +6,29 @@ export class InformationService {
 
   info:any = {};
   load:boolean = false;
+  loadOverUs:boolean = false;
+  team:any[] = [];
 
   constructor( public http:Http) {
+    this.load_info();
+    this.load_over_us();
+  }
+
+  public load_info(){
     this.http.get("assets/data/info.pages.json")
+             .subscribe(data => {
+               //console.log(data.json());
+               this.load = true;
+               this.info = data.json();
+             });
+  }
+
+  public load_over_us(){
+    this.http.get("https://web-page-33deb.firebaseio.com/team.json")
              .subscribe(data => {
                console.log(data.json());
                this.load = true;
                this.info = data.json();
-             })
+             });
   }
-
 }
